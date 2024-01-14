d9d3593ad74baec034b06561bfc8b42a3c34f155
Eric Rochow <=>
2024-01-14 21:19:17 +0000

    feat: utilize different mount points for the api

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,46 @@ fn ping() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/",
-        routes![
-            ping,
-            routes::basic_info::get_basic_info,
-            routes::users::get_all_users,
-            routes::users::get_my_user,
-        ],
-    )
+    rocket::build()
+        .mount("/", routes![ping,])
+        .mount(
+            "/basic_info",
+            routes![
+                routes::basic_info::get_basic_info,
+                routes::basic_info::get_basic_info_fact,
+            ],
+        )
+        .mount(
+            "/certifications",
+            routes![
+                routes::certifications::get_certification,
+                routes::certifications::get_certifications,
+            ],
+        )
+        .mount(
+            "/competencies",
+            routes![routes::competencies::get_competencies,],
+        )
+        .mount(
+            "/education",
+            routes![
+                routes::education::get_education_history,
+                routes::education::get_education_item,
+            ],
+        )
+        .mount(
+            "/experience",
+            routes![routes::experience::get_experience_history,],
+        )
+        .mount(
+            "/preferences",
+            routes![
+                routes::preferences::get_preference,
+                routes::preferences::get_preferences,
+            ],
+        )
+        .mount(
+            "/users",
+            routes![routes::users::get_all_users, routes::users::get_my_user,],
+        )
 }
